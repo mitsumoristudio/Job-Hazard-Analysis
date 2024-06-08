527271770a985954a443401cfcc00e8903d8f977
Satoshi550 <78008562+Satoshi550@users.noreply.github.com>
2024-06-08 10:23:48 +0900

    Initial Commit

---
+++ Job Hazard Analysis/Job_Hazard_AnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Job_Hazard_AnalysisApp.swift
+//  Job Hazard Analysis
+//
+//  Created by Satoshi Mitsumori on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Job_Hazard_AnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
